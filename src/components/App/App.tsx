75d03276e9bd673baaed28d897ee0ef9e5e61e4e
Ann K <kubareva.ann@gmail.com>
2025-06-29 11:34:46 +0300

    ready for review

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,53 @@
+import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
-import { useState } from "react";
 import { Votes, VoteType } from "../../Types/votes";
+import { Notification } from "../Notification/Notification";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
 
 export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const handleVote = (type: VoteType) => {
+    setVotes((prevStats) => ({
+      ...prevStats,
+      [type]: prevStats[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalVotes = votes.bad + votes.good + votes.neutral;
+
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={
+            votes.good === 0 ? 0 : Math.round((votes.good / totalVotes) * 100)
+          }
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 }
